73b3fc515af84ea32d7d8c938342757b72805620
PaulosdOliveira <pauloricardomilos@gmail.com>
2025-06-07 00:33:44 -0300

    Criação do projeto

---
+++ selectAspi/src/main/java/com/github/PaulosdOliveira/TCC/selectAspi/SelectAspiApplication.java
@@ -0,0 +1,13 @@
+package com.github.PaulosdOliveira.TCC.selectAspi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SelectAspiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SelectAspiApplication.class, args);
+	}
+
+}
---
+++ selectAspi/src/test/java/com/github/PaulosdOliveira/TCC/selectAspi/SelectAspiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.PaulosdOliveira.TCC.selectAspi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectAspiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
